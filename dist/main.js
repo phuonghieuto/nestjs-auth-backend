7302d34e8be4802aba92f2095b9e31847d44c152
21120453 <phuonghieuto@gmail.com>
2024-10-30 12:22:03 +0700

    config cors for new frontend deploy url

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,12 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: 'http://localhost:3000',
+        origin: [
+            'http://localhost:3000',
+            'https://nextjs-auth-frontend-git-main-to-phng-hius-projects.vercel.app',
+            'https://nextjs-auth-frontend.vercel.app',
+            'https://nextjs-auth-frontend-boflcxntc-to-phng-hius-projects.vercel.app'
+        ],
         allowedHeaders: '*',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // React frontend URL
+    origin: [
+      'http://localhost:3000',
+      'https://nextjs-auth-frontend-git-main-to-phng-hius-projects.vercel.app',
+      'https://nextjs-auth-frontend.vercel.app',
+      'https://nextjs-auth-frontend-boflcxntc-to-phng-hius-projects.vercel.app'
+    ],
     allowedHeaders: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
